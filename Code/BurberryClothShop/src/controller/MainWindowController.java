b0d35fbdeca6e43bda69eb36ac97f81c1bf463d7
AdhithaV13 <130221684+AdhithaV13@users.noreply.github.com>
2023-10-09 19:38:34 +0530

    first commit

---
+++ Code/BurberryClothShop/src/controller/MainWindowController.java
@@ -0,0 +1,51 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainWindowController {
+
+    @FXML
+    private Button btnClose;
+
+    @FXML
+    private Button btnLogIn;
+
+    @FXML
+    private Button btnSignIn;
+
+    @FXML
+    private AnchorPane mainWindowPane;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnCloseClicked(MouseEvent event) throws IOException {
+        mainWindowPane.setVisible(false);
+    }
+
+    @FXML
+    void btnLogInClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnSignInClicked(MouseEvent event) throws IOException {
+        Stage stage = (Stage) mainWindowPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/SignIn.fxml"))));
+        stage.show();
+    }
+
+}
