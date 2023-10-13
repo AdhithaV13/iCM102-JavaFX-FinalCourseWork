0e6f1b3f3850e0de235deca34ad7946697ab4ffe
AdhithaV13 <130221684+AdhithaV13@users.noreply.github.com>
2023-10-13 11:51:31 +0530

    sixteenth commit

--- Code/BurberryClothShop/src/controller/ForgottenPasswordController.java
+++ Code/BurberryClothShop/src/controller/ForgottenPasswordController.java
@@ -9,7 +9,11 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.io.IOException;
+import java.util.Properties;
 
 public class ForgottenPasswordController {
 
@@ -33,8 +37,28 @@ public class ForgottenPasswordController {
     }
 
     @FXML
-    void btnSendClicked(MouseEvent event) {
+    void btnSendClicked(MouseEvent event) throws MessagingException {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth",true);
+        properties.put("mail.smtp.hot","smtp.gmail.com");
+        properties.put("mail.smtp.port",587);
+        properties.put("mail.smtp.starttls.enable",true);
+        properties.put("mail.transport.protocl","smtp");
 
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("adhithavichak@gmail.com","12#Mmadhitha");
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        message.setSubject("OTP of Burberry Cloth Shop");
+        message.setContent("<h1>Email from fullstack developer Adhitha Vithanage..</h1>","text/html");
+
+        Address addressTo = new InternetAddress(txtEmail.getText());
+        message.setRecipient(Message.RecipientType.TO,addressTo);
+        Transport.send(message);
     }
 
 }
