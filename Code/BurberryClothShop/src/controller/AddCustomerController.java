797a01e0ccf777868fd1e404915d21d4d4ab8eb7
AdhithaV13 <130221684+AdhithaV13@users.noreply.github.com>
2023-10-14 11:30:46 +0530

    twentysixth commit

--- Code/BurberryClothShop/src/controller/AddCustomerController.java
+++ Code/BurberryClothShop/src/controller/AddCustomerController.java
@@ -1,10 +1,16 @@
 package controller;
 
+import db.DBConnection;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class AddCustomerController {
 
@@ -33,8 +39,32 @@ public class AddCustomerController {
     private TextField txtName;
 
     @FXML
-    void btnAddClicked(MouseEvent event) {
+    void btnAddClicked(MouseEvent event) throws SQLException, ClassNotFoundException {
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        String contact = txtContact.getText();
+        String gender = txtGender.getText();
 
+        if(contact.length()==10){
+            if(contact.charAt(0)=='0'){
+                Connection connection = DBConnection.getInstance().getConnection();
+                String sql = "INSERT INTO customers VALUES(?,?,?,?,?)";
+                PreparedStatement pstm = connection.prepareStatement(sql);
+                pstm.setString(1,id);
+                pstm.setString(2,name);
+                pstm.setString(3,address);
+                pstm.setString(4,contact);
+                pstm.setString(5,gender);
+                pstm.executeUpdate();
+
+                new Alert(Alert.AlertType.INFORMATION, "Customer saved successfully..!").show();
+            }else{
+                new Alert(Alert.AlertType.INFORMATION, "Enter valid contact number..!").show();
+            }
+        }else{
+            new Alert(Alert.AlertType.INFORMATION, "Enter valid contact number..!").show();
+        }
     }
 
     @FXML
